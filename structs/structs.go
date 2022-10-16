f5510b0b641f855737e8bf4c00b9dd6b926b4d3b
senpainikolay <nicolae32gherman@gmail.com>
2022-10-16 18:21:28 +0300

    Ensured simple comunication between Client and DininHall

--- structs/structs.go
+++ structs/structs.go
@@ -1,6 +1,12 @@
 package structs
 
-import "sync"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"sync"
+)
 
 type Food struct {
 	Id               int    `json:"id"`
@@ -33,14 +39,57 @@ type MenuGet struct {
 }
 
 type Order struct {
-	RestaurantId int     `json:"restaurant_id"`
-	Items        []int   `json:"items"`
-	Priority     int     `json:"priority"`
-	MaxWait      float64 `json:"max_wait"`
-	CreatedTime  int64   `json:"created_time"`
+	RestaurantId int `json:"restaurant_id"`
+	OrderToDiningHall
+}
+
+type OrderToDiningHall struct {
+	Items       []int   `json:"items"`
+	Priority    int     `json:"priority"`
+	MaxWait     float64 `json:"max_wait"`
+	CreatedTime int64   `json:"created_time"`
 }
 
 type Orders struct {
 	ClientId int     `json:"client_id"`
 	Orders   []Order `json:"orders"`
+}
+
+type ClientOrderResponse struct {
+	RestaurantId         int     `json:"restaurant_id"`
+	OrderId              int     `json:"order_id"`
+	EstimatedWaitingTime float64 `json:"estimated_waiting_time"`
+	CreatedTime          int64   `json:"created_time"`
+	RegisteredTime       int64   `json:"registered_time"`
+}
+
+type OMResponse struct {
+	RestaurantId         int     `json:"restaurant_id"`
+	RestaurantAddress    string  `json:"restaurant_address"`
+	OrderId              int     `json:"order_id"`
+	EstimatedWaitingTime float64 `json:"estimated_waiting_time"`
+	CreatedTime          int64   `json:"created_time"`
+	RegisteredTime       int64   `json:"registered_time"`
+}
+
+type ClientResponse struct {
+	OrderId int          `json:"order_id"`
+	Orders  []OMResponse `json:"orders"`
+}
+
+type Conf struct {
+	Port string `json:"port"`
+}
+
+func GetConf() *Conf {
+	jsonFile, err := os.Open("configurations/Conf.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	var conf Conf
+	json.Unmarshal(byteValue, &conf)
+	return &conf
+
 }
